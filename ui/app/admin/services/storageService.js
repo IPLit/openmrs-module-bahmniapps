99833b1684279845902d7c899ded19701a25b7c3
kushboojain-iplit <kjain@iplit.in>
2024-01-11 12:34:59 +0530

    Showing storage details

--- ui/app/admin/services/storageService.js
+++ ui/app/admin/services/storageService.js
@@ -2,8 +2,11 @@
 
 angular.module('bahmni.admin').service('storageService', ['$rootScope', '$http', '$q', function ($rootScope, $http, $q) {
     this.getUsedSpace = function () {
-        var totalSpace = $rootScope.maxStorageSpace || Bahmni.Common.Constants.maxStorageLimit;
-        var url = Bahmni.Common.Constants.storageUrl + Bahmni.Common.Constants.containerName;
+        $rootScope.totalSpace = $rootScope.maxStorageSpace || Bahmni.Common.Constants.maxStorageLimit;
+        $rootScope.containerNameOrId = Bahmni.Common.Constants.containerName;
+        var url = Bahmni.Common.Constants.storageUrl + $rootScope.containerNameOrId;
+        $rootScope.contactURL = Bahmni.Common.Constants.contactURL;
+        $rootScope.salesURL = Bahmni.Common.Constants.salesURL;
 
         $http.get(url)
             .then(function (response) {
@@ -12,8 +15,8 @@ angular.module('bahmni.admin').service('storageService', ['$rootScope', '$http',
                 var consumedSpaceStr = dockerData.StorageStats.Size;
                 var consumedSpace = consumedSpaceStr.match(/(\d+)/)[0];
                 $rootScope.consumedSpaceText = consumedSpaceStr;
-                $rootScope.totalSpaceText = totalSpace + ' GB';
-                $rootScope.consumedPercentage = (consumedSpace / totalSpace) * 100;
+                $rootScope.totalSpaceText = $rootScope.totalSpace + ' GB';
+                $rootScope.consumedPercentage = parseFloat((consumedSpace / $rootScope.totalSpace) * 100);
             })
             .catch(function (error) {
                 console.error('Error fetching data from Docker API:', error);
